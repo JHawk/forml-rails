3e8aab23e43e31dcda7a15c8ba4a41c595f14b0b
JHawk <josh.r.hawk@gmail.com>
2012-12-30 20:25:56 -0500

    added a test

--- test/lib/forml-rails_test.rb
+++ test/lib/forml-rails_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'rails/generators/rails/controller/controller_generator'
+require 'rails/generators/rails/scaffold/scaffold_generator'
 
 class ControllerGeneratorTest < Rails::Generators::TestCase
   tests Rails::Generators::ControllerGenerator
@@ -17,3 +18,20 @@ class ControllerGeneratorTest < Rails::Generators::TestCase
     assert_file "app/assets/javascripts/abcs.js.forml"
   end
 end
+
+class ScaffoldGeneratorTest < Rails::Generators::TestCase
+  tests Rails::Generators::ScaffoldGenerator
+
+  destination File.expand_path("../tmp", __FILE__)
+  setup do
+    prepare_destination
+    copy_routes
+  end
+
+  def test_assets
+    run_generator %w(blahs --javascript-engine=forml --orm=false)
+    assert_no_file "app/assets/javascripts/blahs.js"
+    assert_file "app/assets/javascripts/blahs.js.forml"
+  end
+end
+
